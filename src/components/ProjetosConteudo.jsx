22506df2d52e5fc42fb9a4994cb13c8e798682cc
Wender Vega <wender_vega@hotmail.com>
2022-06-24 08:59:22 -0300

    correção de imagem

--- src/components/ProjetosConteudo.jsx
+++ src/components/ProjetosConteudo.jsx
@@ -2,31 +2,31 @@ export default function ProjetosConteudo() {
   return (
     <section className="projetosConteudo">
       <div className="divTeste">
-        <img className="imgProjetos" src="./img/projetos/sity.png" alt="" />{" "}
+        <img className="imgProjetos" src="./img/projetos/sity.jpg" alt="" />{" "}
       </div>
       <div className="divTeste">
-        <img className="imgProjetos" src="./img/projetos/sity.png" alt="" />{" "}
+        <img className="imgProjetos" src="./img/projetos/sity.jpg" alt="" />{" "}
       </div>
       <div className="divTeste">
-        <img className="imgProjetos" src="./img/projetos/sity.png" alt="" />{" "}
+        <img className="imgProjetos" src="./img/projetos/sity.jpg" alt="" />{" "}
       </div>
       <div className="divTeste">
-        <img className="imgProjetos" src="./img/projetos/sity.png" alt="" />{" "}
+        <img className="imgProjetos" src="./img/projetos/sity.jpg" alt="" />{" "}
       </div>
       <div className="divTeste">
-        <img className="imgProjetos" src="./img/projetos/sity.png" alt="" />{" "}
+        <img className="imgProjetos" src="./img/projetos/sity.jpg" alt="" />{" "}
       </div>
       <div className="divTeste">
-        <img className="imgProjetos" src="./img/projetos/sity.png" alt="" />{" "}
+        <img className="imgProjetos" src="./img/projetos/sity.jpg" alt="" />{" "}
       </div>
       <div className="divTeste">
-        <img className="imgProjetos" src="./img/projetos/sity.png" alt="" />{" "}
+        <img className="imgProjetos" src="./img/projetos/sity.jpg" alt="" />{" "}
       </div>
       <div className="divTeste">
-        <img className="imgProjetos" src="./img/projetos/sity.png" alt="" />{" "}
+        <img className="imgProjetos" src="./img/projetos/sity.jpg" alt="" />{" "}
       </div>
       <div className="divTeste">
-        <img className="imgProjetos" src="./img/projetos/sity.png" alt="" />{" "}
+        <img className="imgProjetos" src="./img/projetos/sity.jpg" alt="" />{" "}
       </div>
     </section>
   );
