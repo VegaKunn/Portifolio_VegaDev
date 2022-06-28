12d5b4351e26ed66f1c9141afabd9580d9627dc9
Wender Vega <wender_vega@hotmail.com>
2022-06-28 07:11:07 -0300

    limitando os projetos

--- src/components/ProjetosConteudo.jsx
+++ src/components/ProjetosConteudo.jsx
@@ -1,37 +1,39 @@
+import Link from "next/link";
+
 export default function ProjetosConteudo() {
   return (
     <section className="projetosConteudo">
-      <div className="divTeste">
-        <img className="imgProjetos" src="./img/projetos/sity.jpg" alt="" />{" "}
-      </div>
-      <div className="divTeste">
-        <img
-          className="imgProjetos"
-          src="./img/projetos/VegaLivros.jpeg"
-          alt=""
-        />{" "}
-      </div>
-      <div className="divTeste">
-        <img className="imgProjetos" src="./img/projetos/vegaapp.jpeg" alt="" />{" "}
-      </div>
-      <div className="divTeste">
-        <img className="imgProjetos" src="./img/projetos/sity.jpg" alt="" />{" "}
-      </div>
-      <div className="divTeste">
-        <img className="imgProjetos" src="./img/projetos/sity.jpg" alt="" />{" "}
-      </div>
-      <div className="divTeste">
-        <img className="imgProjetos" src="./img/projetos/sity.jpg" alt="" />{" "}
-      </div>
-      <div className="divTeste">
-        <img className="imgProjetos" src="./img/projetos/sity.jpg" alt="" />{" "}
-      </div>
-      <div className="divTeste">
-        <img className="imgProjetos" src="./img/projetos/sity.jpg" alt="" />{" "}
-      </div>
-      <div className="divTeste">
-        <img className="imgProjetos" src="./img/projetos/sity.jpg" alt="" />{" "}
-      </div>
+      <Link href={"https://sitycangirls.vercel.app/"}>
+        <a target="_blank">
+          <div className="divTeste">
+            <img className="imgProjetos" src="./img/projetos/sity.jpg" alt="" />
+          </div>
+        </a>
+      </Link>
+
+      <Link href={"https://github.com/VegaKunn/testBook"}>
+        <a target="_blank">
+          <div className="divTeste">
+            <img
+              className="imgProjetos"
+              src="./img/projetos/VegaLivros.jpeg"
+              alt=""
+            />
+          </div>
+        </a>
+      </Link>
+
+      <Link href={"https://github.com/VegaKunn/PortifolioApp"}>
+        <a target="_blank">
+          <div className="divTeste">
+            <img
+              className="imgProjetos"
+              src="./img/projetos/vegaapp.jpeg"
+              alt=""
+            />
+          </div>
+        </a>
+      </Link>
     </section>
   );
 }
